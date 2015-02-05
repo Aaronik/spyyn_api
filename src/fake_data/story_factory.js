43e13bbc35df2917f300defb4e3b8019ff70ff3e
Aaron Sullivan <aasullivan1618@gmail.com>
2015-02-04 21:09:25 -0800

    Adds idiosyncracy credit to metrics closes https://github.com/Aaronik/spyyn_api/issues/2

--- src/fake_data/story_factory.js
+++ src/fake_data/story_factory.js
@@ -9,7 +9,8 @@ module.exports = function storyFactory (distinguishingNum) {
       internalImportance: distinguishingNum % 100,
       sentiment: (distinguishingNum + 70) % 100,
       focus: (distinguishingNum + 50) % 100,
-      controversy: (distinguishingNum + 30) % 100
+      controversy: (distinguishingNum + 30) % 100,
+      idiosyncracyCredit: distinguishingNum + 23 % 100
     },
     id: distinguishingNum,
     title: ['these', 'words', 'will', 'never', 'change']
