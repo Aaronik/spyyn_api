33e42fb6062293e846cc1cac3c708e962ba155e9
Aaron Sullivan <aasullivan1618@gmail.com>
2015-02-15 19:09:33 -0800

    adds desired routes w/ dummy functions

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,24 @@
 var fakeServer = require('./fake_server');
 
 module.exports = function (app) {
-  app.get('/fake-server/journals/:journalId/history', fakeServer.history);
-  app.get('/fake-server/stories/:storyId', fakeServer.stories);
+  // all stories (paged by date?)
+  // story objects, each containing a bunch of articles
+  app.get('/fake-server/stories', function());
+  app.get('/fake-server/stories/:storyId', fakeServer.stories); // come with many articles, which each have a journal
+
+  // noop
+  app.get('/fake-server/articles', function());
+  // one article, one journal, many stories
+  app.get('/fake-server/articles/:articleId', function());
+
+  // return all currently used journal metadata
+  app.get('/fake-server/journals', function());
+  // metadata about the journal
+  app.get('/fake-server/journals/:journalId', function());
+  // many articles
+  app.get('/fake-server/journals/:journalId/history', fakeServer.history); 
+  // many articles, related to one story
+  app.get('/fake-server/journals/:journalId/story/:storyId', function());
 }
+
 
